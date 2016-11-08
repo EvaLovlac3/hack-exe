4dfb55a002fcda1c81057f0ab1c7861b13b7fd0e
Rubén de Celis Hernández <contact@rdch106.hol.es>
2016-11-08 11:32:48 +0100

    Fixed #3 ANSI color in Windows 10 version 10.0.14393 (Windows Anniversary Update)
    https://gist.github.com/RDCH106/6562cc7136b30a5c59628501d87906f7

--- hack_exe/hack_exe.py
+++ hack_exe/hack_exe.py
@@ -3,6 +3,14 @@ import random
 import socket
 import sys
 import time
+import os
+import platform
+
+if os.name == 'nt' and platform.release() == '10' and platform.version() >= '10.0.14393':
+    # Fix ANSI color in Windows 10 version 10.0.14393 (Windows Anniversary Update)
+    import ctypes
+    kernel32 = ctypes.windll.kernel32
+    kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
 clear = '\033[0m'
 red = '\033[31m'
